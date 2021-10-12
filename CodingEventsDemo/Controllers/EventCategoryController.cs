38ae6dceb6639daf358cb97e0ddb8fe765f32cea
Sabeena <sabeenarayyan13@gmail.com>
2021-10-12 16:22:54 -0500

    added the second db

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using CodingEventsDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext category;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            category = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
